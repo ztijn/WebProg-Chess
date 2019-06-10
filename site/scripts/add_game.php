003d71b995b4298052892ef3587003accc491aa8
robinsnoek <49788576+robinsnoek@users.noreply.github.com>
2019-06-10 17:55:36 +0200

    New game werkt nu weer

--- site/scripts/add_game.php
+++ site/scripts/add_game.php
@@ -1,34 +1,37 @@
 <?php
-session_start();
-if (isset($_POST['call_now'])) {
+if (isset($_POST['submit'])) {
     //Send game id with get
+    $args = array(
+        'game_id' => $_POST['gameid']
+    );
     // Read games
     $json_file = file_get_contents("../data/checkers.json");
     $games = json_decode($json_file, true);
     // Generate game ID
-    $game_id = $_SESSION['game_id'];
+    $game_id = $_POST['gameid'];
     foreach ($games as $key => $value){
         if ($game_id == $value['game_id']) {
-            $game_key = $key;
+            header("Location: ../checkers.php?". http_build_query($args));
+            die();
         };
     }
-    $games[$game_key]= [
+    array_push($games, [
         'game_id' => $game_id,
         'white' => ["A9", "C9", "E9", "G9", "I9", "B10", "D10", "F10", "H10", "J10",
-            "A7", "C7", "E7", "G7", "I7", "B8", "D8", "F8", "H8", "J8"],
+    "A7", "C7", "E7", "G7", "I7", "B8", "D8", "F8", "H8", "J8"],
         'black' => ["A1", "C1", "E1", "G1", "I1", "B2", "D2", "F2", "H2", "J2",
-            "A3", "C3", "E3", "G3", "I3", "B4", "D4", "F4", "H4", "J4"],
+    "A3", "C3", "E3", "G3", "I3", "B4", "D4", "F4", "H4", "J4"],
         "white_king" => [""],
         "black_king"=> [""],
         "turn" => array(
             "black_turn" => 1,
             "white_turn:" => 1
-        )];
+)]);
     // Save game to external file
     $json_file = fopen('../data/checkers.json', 'w');
     fwrite($json_file, json_encode($games));
     fclose($json_file);
     // Redirect to game page
-//    header("Location: ../checkers.php?". http_build_query($args));
+    header("Location: ../checkers.php?". http_build_query($args));
     die();
 }
